bf3c3ee3e6ece27c91a740fbcb2078a42fe74db0
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-01 18:46:41 -0700

    parcel capabilities, end of chapter-1: Igniting the app

--- App.js
+++ App.js
@@ -1,5 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+/*
+* creates a server
+* Hot Module Replacement
+* .parcel-cache
+* bundling
+* minify
+* cleaning our code
+* dev and production build
+* image optimization
+* caching while development
+* compression
+* compatible with older version of browser
+* HTTPS on dev
+* port number
+* consistent hashing algorithm
+* zero config
+*/
 
 const heading1 = React.createElement("h1", { id: 'title' }, "Heading1!")
 const heading2 = React.createElement("h2", { id: 'title' }, "Heading2")
