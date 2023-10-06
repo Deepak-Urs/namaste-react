dea65ed3332994950098208a290180efa3d07289
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-05 19:59:42 -0700

    JSX introduction

--- App.js
+++ App.js
@@ -19,11 +19,15 @@ import ReactDOM from "react-dom/client"
 * Tree Shaking - Removing unwanted code
 */
 
-const heading1 = React.createElement("h1", { id: 'title' }, "Heading1!")
-const heading2 = React.createElement("h2", { id: 'title' }, "Heading2")
-const container = React.createElement("div", { id: 'container' }, [heading1, heading2])
+//const heading1 = React.createElement("h1", { id: 'title', key: 'h1' }, "Heading1!")
+//const heading2 = React.createElement("h2", { id: 'title', key: 'h2' }, "Heading2")
+//const container = React.createElement("div", { id: 'container' }, [heading1, heading2])
+
+const headingJSX = (
+    <h1 title="id">Namaste Javascript</h1>
+)
+
+console.log('headingJSX', headingJSX);
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(container)
-
-console.log(heading1);
+root.render(headingJSX)
