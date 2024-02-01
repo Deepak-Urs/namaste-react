e55752228248f715dfcf9f43757427079afe22a8
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-01 14:24:24 -0800

    vanilla React dev using CDN

---
+++ practice/index.js
@@ -0,0 +1,9 @@
+console.log('js linked');
+
+const heading = React.createElement("h1", {}, "Namaste Everyone!")
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(heading)
+
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(container)
