d99ef35cc44f50c6c1c9d2d6ba9cac88c8159328
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-02 14:04:38 -0800

    modern parcel basic start

--- practice/index.js
+++
@@ -1,9 +0,0 @@
-console.log('js linked');
-
-const heading = React.createElement("h1", {}, "Namaste Everyone!")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
-
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(container)
