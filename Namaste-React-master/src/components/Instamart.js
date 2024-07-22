5e24187dcc75e5f7a94e2fb4231cc9b086075dc6
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-07-22 13:53:05 -0700

    bug fix in progress

--- Namaste-React-master/src/components/Instamart.js
+++ Namaste-React-master/src/components/Instamart.js
@@ -10,42 +10,50 @@ const Section = ({title, desc, isVisible, setIsVisible}) => {
     )
 }
 const Instamart = () => {
-    const [sectionConfig, setSectionConfig] = useState({
-        showAbout: false,
-        showTeam: false,
-        showCareers: true
-    });
+    const [visibleSection, setVisibleSection] = useState("");
     return (
         <div>
             <h1 className="text-3xl p-2 m-2 font-bold">Instamart</h1>
 
             <Section title={"About Instamart"}
                 desc={"This is About section of Instamart"} 
-                isVisible={sectionConfig.showAbout}
-                setIsVisible={() => setSectionConfig({
-                    showAbout: true,
-                    showTeam: false,
-                    showCareers: false
-                })}
+                isVisible={visibleSection === "about"}
+                setIsVisible={() => {
+                    if(visibleSection !== "about") {
+                        setVisibleSection("about")
+                    }
+                    else {
+                        //setVisibleSection("")
+                        //Section.isVisible = false
+                        //setVisibleSection(false)
+                    }}}
                 />
 
             <Section title={"About Instamart"}
                 desc={"This is Team section of Instamart"} 
-                isVisible={sectionConfig.showTeam}
-                setIsVisible={() => setSectionConfig({
-                    showAbout: false,
-                    showTeam: true,
-                    showCareers: false
-                })}/>
+                isVisible={visibleSection === "team"}
+                setIsVisible={() => {
+                    if(visibleSection !== "team") {
+                        setVisibleSection("team")
+                    }
+                    else {
+                        //Section.isVisible = false
+                        //setVisibleSection("")
+                        //setVisibleSection(false)
+                    }}}/>
             
             <Section title={"Careers"}
                 desc={"This is Career section of Instamart"} 
-                isVisible={sectionConfig.showCareers}
-                setIsVisible={() => setSectionConfig({
-                    showAbout: false,
-                    showTeam: false,
-                    showCareers: true
-                })}/>
+                isVisible={visibleSection === "careers"}
+                setIsVisible={() => {
+                    if(visibleSection !== "careers") {
+                        setVisibleSection("careers")
+                    }
+                    else {
+                        //Section.isVisible = false
+                        //setVisibleSection("")
+                        //setVisibleSection(false)
+                    }}}/>
         </div>
     )
 }
