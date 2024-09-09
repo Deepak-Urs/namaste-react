d92731687b6c2721f09b4c7deef61210c4cdc226
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-09-08 20:35:16 -0700

    basic tests

--- Namaste-React-master/src/components/__tests__/sum.test.js
+++ Namaste-React-master/src/components/__tests__/sum.test.js
@@ -1,14 +1,14 @@
 import { sum } from "../sum" // throws error as JS doesn't understand import
 
-function s2 (a, b) {
-    return a * b;
-}
+//function s2 (a, b) {
+//    return a * b;
+//}
 
-//test("check sum of two numbers", () => {
-//    //assertion
-//    expect(sum(2, 5).toBe(7));
-//})
-
-test("s2", () => {
-    expect(s2(2,3).toBe(6));
+test("check sum of two numbers", () => {
+    //assertion
+    sum(2,5).toEqual(0)
 })
+
+//test("s2", () => {
+//    expect(s2(2,3).toBe(6));
+//})
